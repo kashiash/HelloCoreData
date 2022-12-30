4f6cd31ef38caf3210430bca01e20858af4ad326
Jacek Kosinski <kashiash@gmail.com>
2022-12-31 00:24:42 +0100

    running version

--- HelloCoreData/ContentView.swift
+++ HelloCoreData/ContentView.swift
@@ -9,15 +9,45 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    let storageProvider: StoragePrivider
+    let storageProvider: StorageProvider
+
+    @State var movieTitle = ""
+    @State var movies: [Movie] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+      VStack {
+        Text("Add a movie")
+          .font(.title)
+          .padding()
+
+        TextField("Movie name", text: $movieTitle)
+          .padding()
+
+        HStack {
+          Button("Save movie") {
+            storageProvider.saveMovie(named: movieTitle)
+          }
+
+          Spacer()
+
+          Button("Fetch all movies") {
+            movies = storageProvider.getAllMovies()
+          }
+        }.padding()
+
+        List {
+          ForEach(movies) { (movie: Movie) in
+            Text(movie.name ?? "")
+          }.onDelete { rows in
+            for row in rows {
+              let movie = movies[row]
+              storageProvider.deleteMovie(movie)
+            }
+
+            movies.remove(atOffsets: rows)
+          }
         }
-        .padding()
+      }
     }
 }
 
--- HelloCoreData/StorageProvider.swift
+++ HelloCoreData/StorageProvider.swift
@@ -8,10 +8,10 @@
 import Foundation
 import CoreData
 
-class StoragePrivider {
+class StorageProvider {
     let persistentContainer: NSPersistentContainer
     init() {
-        persistentContainer = NSPersistentContainer(name: "HelloCoreData")
+        persistentContainer = NSPersistentContainer(name: "HelloCoreDataModel")
         
         persistentContainer.loadPersistentStores(completionHandler: {
             description, error in
@@ -20,4 +20,51 @@ class StoragePrivider {
             }
         })
     }
+    func saveMovie(named name: String) {
+      let movie = Movie(context: persistentContainer.viewContext)
+      movie.name = name
+
+      do {
+        try persistentContainer.viewContext.save()
+        print("Movie saved succesfully")
+      } catch {
+        persistentContainer.viewContext.rollback()
+        print("Failed to save movie: \(error)")
+      }
+    }
+    
+    func getAllMovies() -> [Movie] {
+      let fetchRequest: NSFetchRequest<Movie> = Movie.fetchRequest()
+
+      do {
+        return try persistentContainer.viewContext.fetch(fetchRequest)
+      } catch {
+        print("Failed to fetch movies: \(error)")
+        return []
+      }
+    }
+    
+    func deleteMovie(_ movie: Movie) {
+      persistentContainer.viewContext.delete(movie)
+
+      do {
+        try persistentContainer.viewContext.save()
+      } catch {
+        persistentContainer.viewContext.rollback()
+        print("Failed to save context: \(error)")
+      }
+    }
+
+    func updateMovie(_ movie: Movie) {
+      do {
+        try persistentContainer.viewContext.save()
+      } catch {
+        persistentContainer.viewContext.rollback()
+        print("Failed to save context: \(error)")
+      }
+    }
 }
+
+
+
+
